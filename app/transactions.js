ef72ce051ea6bc4efb6322235719db38e1637e27
Jovi <kwong.jovi@gmail.com>
2022-10-24 16:18:40 +0100

    feat(Transactions): test-drive withdraw() - pass initial basic unit tests

--- app/transactions.js
+++ app/transactions.js
@@ -1,17 +1,23 @@
 class Transactions {
   constructor() {
     this.transactionsList = [];
-    // this.currentDate = ""
   }
 
   list() {
     if (this.transactionsList.length === 0) {
       return "No transactions yet.";
     }
+    console.log("Current transactions: " + this.transactionsList);
     return this.transactionsList;
   }
 
   deposit(date, amount) {
+    const transaction = {};
+    transaction[date] = amount;
+    this.transactionsList.push(transaction);
+  }
+
+  withdraw(date, amount) {
     const transaction = {};
     transaction[date] = amount;
     this.transactionsList.push(transaction);
--- spec/transactions.test.js
+++ spec/transactions.test.js
@@ -1,8 +1,6 @@
 const Transactions = require("../app/transactions");
 
 describe("Transactions", () => {
-  // Use beforeEach for repeated actions
-
   describe("push transactions as key/value pairs to array with amount and date", () => {
     it("#list returns default message before any transacitons ", () => {
       const transactions = new Transactions();
@@ -21,6 +19,20 @@ describe("Transactions", () => {
       expect(transactions.list()).toEqual([
         { "01/01/2022": 1.0 },
         { "02/01/2022": 2.0 },
+      ]);
+    });
+
+    it("#withdraw pushes negative transaction(s) to array; #list returns array of transactions", () => {
+      const transactions = new Transactions();
+
+      expect(transactions.list()).toEqual("No transactions yet.");
+
+      transactions.withdraw("01/01/2022", -1.0);
+      transactions.withdraw("02/01/2022", -2.0);
+
+      expect(transactions.list()).toEqual([
+        { "01/01/2022": -1.0 },
+        { "02/01/2022": -2.0 },
       ]);
     });
   });
