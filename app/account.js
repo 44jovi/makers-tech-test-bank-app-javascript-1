7d549ecfd25ca61d84e8df9c47bf2fd1bbeec480
Jovi <kwong.jovi@gmail.com>
2022-10-25 00:32:21 +0100

    feat(Account): test-drive withdraw(). Pass basic initial unit tests

--- app/account.js
+++ app/account.js
@@ -1,12 +1,17 @@
-const Balance = require("./balance");
-
 class Account {
   constructor(balance) {
     this.balance = balance;
   }
 
+  // Deposit amount must be input as a positive number (float)
   deposit(amount) {
     this.balance.update(amount);
+  }
+
+  // Withdrawl amount must be input as a positive number (float)
+  withdraw(amount) {
+    // Convert amount into negative float
+    this.balance.update(-Math.abs(amount));
   }
 }
 
--- spec/account.test.js
+++ spec/account.test.js
@@ -6,18 +6,19 @@ jest.mock("../app/balance");
 beforeEach(() => {
   mockBalance = new Balance();
   account = new Account(mockBalance);
-  // Balance.mockClear();
 });
 
 describe("Account", () => {
   describe("deposit()", () => {
     it("each execution adds an amount to account balance", () => {
+      // For now mimic the instance variable `this.currentBalance` in the Balance class
       let mockCurrentBalance = 0.0;
 
       mockBalance.update.mockImplementation((amount) => {
         mockCurrentBalance += amount;
       });
 
+      expect(mockCurrentBalance).toEqual(0.0);
       account.deposit(1.0);
       expect(mockCurrentBalance).toEqual(1.0);
       account.deposit(2.0);
@@ -25,5 +26,24 @@ describe("Account", () => {
     });
   });
 
-  // Add tests for invalid input / errors
+  describe("withdraw()", () => {
+    it("each execution subtracts an amount from account balance", () => {
+      // For now mimic the instance variable `this.currentBalance` in the Balance class
+      let mockCurrentBalance = 0.0;
+
+      mockBalance.update.mockImplementation((amount) => {
+        mockCurrentBalance += amount;
+      });
+
+      expect(mockCurrentBalance).toEqual(0.0);
+      account.withdraw(1.0);
+      expect(mockCurrentBalance).toEqual(-1.0);
+      account.withdraw(2.0);
+      expect(mockCurrentBalance).toEqual(-3.0);
+    });
+  });
+
+  // Add tests for printing statement
+
+  // Add tests for invalid input and error throws for all methods
 });
