1fb6e244a8d63aa77ba42858006d243c89bbbefe
Jovi <kwong.jovi@gmail.com>
2022-10-25 17:02:52 +0100

    fix(Account): amend constructor (this.transactions) and update printStatement(). Pass unit tests

--- app/account.js
+++ app/account.js
@@ -1,19 +1,31 @@
 class Account {
   constructor(balance, transactions) {
     this.balance = balance;
-    this.transactionsList = transactions.list();
+    this.transactions = transactions;
     this.statementHeader = "date || credit || debit || balance";
   }
 
   // Deposit amount must be input as a positive number (float)
   deposit(amount) {
     this.balance.update(amount);
+    const date = new Date().toLocaleDateString("en-GB");
+    this.transactions.add(date, amount);
   }
 
   // Withdrawl amount must be input as a positive number (float)
   withdraw(amount) {
-    // Convert amount into negative float
-    this.balance.update(-Math.abs(amount));
+    // If amount is positive, convert it to negative
+    if (Math.sign(amount) === 1) {
+      amount = -Math.abs(amount);
+    }
+    // If amount is negative or zero, leave it unchanged
+    else if (Math.sign(amount) === -1) {
+      amount = amount;
+    }
+
+    this.balance.update(amount);
+    const date = new Date().toLocaleDateString("en-GB");
+    this.transactions.add(date, amount);
   }
 
   printStatement() {
@@ -21,7 +33,7 @@ class Account {
 
     // WIP: Fix Prettier auto-formatting here
     // Fix number outputs to 2 decimal places
-    this.transactionsList.forEach((transaction) => {
+    this.transactions.list().forEach((transaction) => {
       statement += `\n${
         transaction.transacDate
       } || ${transaction.credit.toFixed(2)} || ${transaction.debit.toFixed(
